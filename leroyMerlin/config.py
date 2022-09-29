b997fab272f9a2da772809177a8669ca3e883fd4
Vadim <transit010@mail.ru>
2022-09-29 21:40:19 +1000

    Для повышения отказоустойчивости в config.py добавлены значения переменных окружения по умолчанию 2

--- leroyMerlin/config.py
+++ leroyMerlin/config.py
@@ -3,9 +3,9 @@ import os
 
 load_dotenv()
 
-PROXY_HOST = os.getenv('PROXY_HOST', '')
-PROXY_PORT = os.getenv('PROXY_PORT', '')
-PROXY_USER = os.getenv('PROXY_USER', '')
-PROXY_PASS = os.getenv('PROXY_PASS', '')
+PROXY_HOST = os.getenv('PROXY_HOST', '127.0.0.0')
+PROXY_PORT = os.getenv('PROXY_PORT', '8080')
+PROXY_USER = os.getenv('PROXY_USER', 'user')
+PROXY_PASS = os.getenv('PROXY_PASS', 'pass')
 MIN_DELAY = int(os.getenv('MIN_DELAY', 8))
 MAX_DELAY = int(os.getenv('MAX_DELAY', 16))
